5a49636e57d33e837a298c00c1bc3fd26a7f0aa0
ssethia <ssethia@62518-ssethia.itrnetwork.com>
2018-04-21 17:57:35 -0700

    First Commit

---
+++ src/main/java/com/poc/springbootstarter/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.poc.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+		
+		@RequestMapping(value="/hello", method=RequestMethod.GET)
+		public String sayHi() {
+			return "Hi";
+		}
+}
